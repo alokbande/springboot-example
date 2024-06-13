8474a91114c23a7c76303ace70645e0762e14c73
shantu.bande@gmail.com <shantu.bande@gmail.com>
2024-06-13 16:14:59 -0400

    commit

--- src/main/java/com/alok/SpringbootExampleApplication.java
+++ src/main/java/com/alok/SpringbootExampleApplication.java
@@ -2,9 +2,18 @@ package com.alok;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringbootExampleApplication {
+	
+	@GetMapping("/test")
+	public String getMethodName() {
+		return "Test AWS";
+	}
+	
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootExampleApplication.class, args);
